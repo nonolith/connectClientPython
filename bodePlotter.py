d6d456289de419e751f96de152927472eb3c8bf9
Ian Daniher <it.daniher@gmail.com>
2012-03-08 12:31:19 -0600

    clean and comment

--- bodePlotter.py
+++ bodePlotter.py
@@ -1,4 +1,4 @@
-from numpy import logspace, log10, mean, linspace, ceil
+from numpy import logspace, log10, mean
 from connectClient import CEE
 import pylab
 import time
@@ -13,35 +13,35 @@ minFreq = 10
 maxFreq = (1/sampleTime)/20
 # maximum frequency = sampling rate / 20
 
-frequencies = logspace(log10(minFreq), log10(maxFreq), 40)
-#log-spaced array of frequencies
+frequencies = logspace(log10(minFreq), log10(maxFreq), 100)
+# log-spaced array of frequencies
 amplitudes = []
 phases = []
 
 def findPhases(maxesA, maxesB):
 	phases = []
-	maxesA = maxesA[1::]
-	maxesB = maxesB[1::]
 	for i in range(len(maxesA)):
-		phase = ( maxesA[i] - maxesB[i] ) * frequency * 360.0
+		phase = ( maxesA[i] - maxesB[i] ) * sampleTime * frequency * 360.0
+		# calculate phase
 		if phase > 180:
 			phase = 180 - phase
 		if phase < -180:
 			phase = 360 + phase
+		# normalize phase
 		phases.append(phase)
 	return phases 
 
-def findLocalMaxes(values, a):
+def findLocalMaxes(values):
 	chunkwidth = len(values)/periodCount
 	# determine samples per period
 	split = [values[i:i+chunkwidth] for i in range(0, len(values), chunkwidth)]
 	# split into periods
 	localMaxes = map(max, split)
 	# find local maximums
-	localMaxTimes = [ (chunk.index(localMax) + split.index(chunk)*chunkwidth) * sampleTime
+	localMaxTimes = [ (chunk.index(localMax) + split.index(chunk)*chunkwidth)
 		for localMax, chunk in zip(localMaxes, split) ]
 	# find indexes of local maximums
-	return localMaxes, localMaxTimes
+	return localMaxes[1::], localMaxTimes[1::]
 	# trim the garbage datapoints and return the information
 
 for frequency in frequencies:
@@ -50,12 +50,11 @@ for frequency in frequencies:
 	# source sine wave with full-scale voltage range at target frequency
 	sampleCount = ( period * periodCount ) / sampleTime
 	# do math to get the equivalent of 'periodCount' in samples
-	startSample = ceil(setResponse['startSample']/period)*period
-	v = CEE.getInput('a', 0, int(sampleCount), int(startSample))[0]
-	i = CEE.getInput('b', 0, int(sampleCount), int(startSample))[0]
+	v = CEE.getInput('a', 0, int(sampleCount), setResponse['startSample'])[0]
+	i = CEE.getInput('b', 0, int(sampleCount), setResponse['startSample'])[0]
 	# get samples from CEE
-	vMaxes, vMaxTimes = findLocalMaxes(v, True)
-	iMaxes, iMaxTimes = findLocalMaxes(i, False)
+	vMaxes, vMaxTimes = findLocalMaxes(v)
+	iMaxes, iMaxTimes = findLocalMaxes(i)
 	# get timestamps and maximum values for voltage and current
 	amplitudes.append(mean(iMaxes))
 	# calculate and record amplitude from array of local maximum currents
